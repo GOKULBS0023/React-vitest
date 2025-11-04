a89ec716c45dce8338cf11c94a6fdad40aa464cf
Gokulbharathi S <gokulbs0023@gmail.com>
2025-11-04 08:45:58 +0530

    Basic testings

--- src/components/Greeting.test.jsx
+++ src/components/Greeting.test.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
-import { describe, it, expect } from "vitest";
+import { it, describe, expect } from "vitest";
 import "@testing-library/jest-dom/vitest";
 
 describe("Greeting", () => {
-  it("renders a default greeting", () => {
-    render(<Greeting />);
+  it("render default greetings", () => {
+    render(
+      <>
+        <Greeting />
+      </>
+    );
     expect(screen.getByText("Hello, World!")).toBeInTheDocument();
   });
-
-  it("renders greeting with a name", () => {
-    render(<Greeting name={"Pedro"} />);
-    const text = screen.getByText("Hello, Pedro!");
+  it("render greetings with name", () => {
+    render(
+      <>
+        <Greeting name={"Gokul"} />
+      </>
+    );
+    const text = screen.getByText("Hello, Gokul!");
     expect(text).toBeInTheDocument();
   });
 });
